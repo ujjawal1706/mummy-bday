5c92ec20aef45baf4e9a4b6ee5abc9a6cd49e8f9
ujjawal1706 <56977641+ujjawal1706@users.noreply.github.com>
2021-08-13 16:50:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const sliderContainer = document.querySelector('.slider-container')
 const slideRight = document.querySelector('.right-slide')
 const slideLeft = document.querySelector('.left-slide')
@@ -15,18 +14,52 @@ downButton.addEventListener('click', () => changeSlide('down'))
 
 const changeSlide = (direction) => {
     const sliderHeight = sliderContainer.clientHeight
-    if(direction === 'up') {
+    if (direction === 'up') {
         activeSlideIndex++
-        if(activeSlideIndex > slidesLength - 1) {
+        if (activeSlideIndex > slidesLength - 1) {
             activeSlideIndex = 0
         }
-    } else if(direction === 'down') {
+    } else if (direction === 'down') {
         activeSlideIndex--
-        if(activeSlideIndex < 0) {
+        if (activeSlideIndex < 0) {
             activeSlideIndex = slidesLength - 1
         }
     }
 
     slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
     slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
+
+function random(num) {
+    return Math.floor(Math.random() * num)
+}
+
+function getRandomStyles() {
+    var r = random(255);
+    var g = random(255);
+    var b = random(255);
+    var mt = random(200);
+    var ml = random(50);
+    var dur = random(5) + 5;
+    return `
+    background-color: rgba(${r},${g},${b},0.7);
+    color: rgba(${r},${g},${b},0.7); 
+    box-shadow: inset -7px -3px 10px rgba(${r-10},${g-10},${b-10},0.7);
+    margin: ${mt}px 0 0 ${ml}px;
+    animation: float ${dur}s ease-in infinite
+    `
+}
+
+function createBalloons(num) {
+    var balloonContainer = document.getElementById("balloon-container")
+    for (var i = num; i > 0; i--) {
+        var balloon = document.createElement("div");
+        balloon.className = "balloon";
+        balloon.style.cssText = getRandomStyles();
+        balloonContainer.append(balloon);
+    }
+}
+
+window.onload = function() {
+    createBalloons(100);
 }
